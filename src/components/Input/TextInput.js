7ec57fad4f8ed24eaf02968041de5eefeb4e43b2
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-03-22 12:27:41 +0530

    Updated the display of the label and input below and above the medium screens.

--- src/components/Input/TextInput.js
+++ src/components/Input/TextInput.js
@@ -1,16 +1,25 @@
 import React, { useId } from "react";
+import { useMediaQuery } from "../../hooks";
 
 import "./TextInput.css";
 
 const TextInput = ({ label, value, updateValue }) => {
+  const isAboveMediumScreen = useMediaQuery("(min-width: 768px)");
   const id = useId();
 
   const onChange = (event) => updateValue(event.target.value);
 
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type="text" value={value} onChange={onChange} />
+      {isAboveMediumScreen && <label htmlFor={id}>{label}</label>}
+      <input
+        id={id}
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={!isAboveMediumScreen ? label : ""}
+        aria-label={label}
+      />
     </>
   );
 };
